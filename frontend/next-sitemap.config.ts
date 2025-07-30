61cd775d3e897e1bdc823b1a8ed36feaab57c299
alikansoh <alikanso.dev@gmail.com>
2025-07-30 13:49:19 +0100

    sitemap added

--- frontend/next-sitemap.config.ts
+++ frontend/next-sitemap.config.ts
@@ -8,19 +8,7 @@ const config: IConfig = {
   priority: 0.7,
   sitemapSize: 5000,
   exclude: ['/admin/*', '/api/*', '/private/*'],
-  additionalPaths: async (config) => [
-    await config.transform(config, '/services'),
-    await config.transform(config, '/about'),
-    await config.transform(config, '/contact'),
-    await config.transform(config, '/projects'), 
-    await config.transform(config, '/services/home-renovations'),
-    await config.transform(config, '/services/bathroom-upgrades'),
-    await config.transform(config, '/services/loft-conversions'),
-    await config.transform(config, '/kitchen-remodeling'),
-    await config.transform(config, '/services/Commercial-Fit-Outs'),
-    await config.transform(config, '/Electrical-services'),
-    await config.transform(config, '/Plumbing-services'),
-  ],
+  
   robotsTxtOptions: {
     policies: [
       {
